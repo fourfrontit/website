c54ddb09a7e81439d54ab576a6759e55e3c9fbdd
fourfrontit <fourfrontit@gmail.com>
2025-10-19 16:13:53 +0530

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,8 +1,9 @@
-import React from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'         // Tailwind + base css
-import { BrowserRouter, Routes, Route } from 'react-router-dom'
-import FourFrontITLanding, { AboutPage } from './four_front_itlanding_preview.jsx'
+// src/main.jsx
+import React from "react";
+import { createRoot } from "react-dom/client";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import "./index.css"; // Tailwind + base css
+import FourFrontITLanding, { AboutPage } from "./four_front_itlanding_preview.jsx";
 
 function AppRoutes() {
   return (
@@ -10,12 +11,10 @@ function AppRoutes() {
       <Routes>
         <Route path="/" element={<FourFrontITLanding />} />
         <Route path="/about" element={<AboutPage />} />
-        {/* add other routes here as needed */}
       </Routes>
     </BrowserRouter>
   );
 }
 
-const root = createRoot(document.getElementById('root'))
-root.render(<AppRoutes />)
-
+const root = createRoot(document.getElementById("root"));
+root.render(<AppRoutes />);
