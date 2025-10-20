b2fa75e2eee7c8a384abc2d0181d447844618040
fourfrontit <fourfrontit@gmail.com>
2025-10-20 12:01:43 +0530

    Delete src directory

--- src/main.jsx
+++
@@ -1,7 +0,0 @@
-import React from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'         // Tailwind + base css
-import FourFrontITLanding, { AboutPage } from './four_front_itlanding_preview.jsx'
-
-const root = createRoot(document.getElementById('root'))
-root.render(<FourFrontITLanding />)
